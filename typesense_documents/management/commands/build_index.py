73ddefecf803f63bb709f09d93e300a4fff830d6
Pavel Zarubin <p.zarubin@stik.pro>
2025-07-24 11:00:01 +0300

    added use_batch to command

--- typesense_documents/management/commands/build_index.py
+++ typesense_documents/management/commands/build_index.py
@@ -5,6 +5,12 @@ from typesense_documents.registry import typesense_registry
 class Command(BaseCommand):
     help = "Create typesense collections and fill"
 
+    def add_arguments(self, parser):
+        parser.add_argument("--use-batch", action="store_true",help="Use batches for update")
+
     def handle(self, *args, **options):
+        use_batch = False
+        if options["use_batch"]:
+            use_batch = True
         for document in typesense_registry.index:
-            document().init_collection()
+            document().init_collection(use_batch=use_batch)
